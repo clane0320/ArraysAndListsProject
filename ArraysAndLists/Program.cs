7042388e7730af7541729408685cc12f7980a745
Chris Lane <clane0320@gmail.com>
2023-01-11 20:39:48 -0600

    excercise 2

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -27,6 +28,45 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+
+            int[] myIntList = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evens = new int[] {2,4,6,8,10};
+            var odds = new int[] { 1, 3, 5, 7, 9 };
+
+            
+
+            Console.WriteLine("Enter a number between 1-10");
+
+            
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num % 2 == 0)
+                
+                Console.WriteLine("is even");
+
+            else
+                
+                Console.WriteLine("is odd");
+
+            
+            Console.WriteLine("List of Evens");
+
+            foreach (int i in evens)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.WriteLine("List of Odds");
+
+           foreach (int i in odds) 
+            {
+            Console.WriteLine(i); 
+
+            }
+
+        
         }
     }
 }
